bc17fc8242d7e5f4d64a44d294d561f62d239b14
elias-m25 <131762074+elias1991xNataly@users.noreply.github.com>
2024-07-11 14:11:53 +0200

    adding publishable key

--- deliveryfoodeas/app/layout.js
+++ deliveryfoodeas/app/layout.js
@@ -22,7 +22,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
 
 
 
